5497a9ee430141f6c151e573cba84399e4907d7d
stevenokennedy <steven.okennedy@gmail.com>
2016-03-15 21:34:57 +0000

    working

--- src/main/java/service/impl/SysAdminServiceImpl.java
+++ src/main/java/service/impl/SysAdminServiceImpl.java
@@ -1,7 +1,10 @@
 package service.impl;
 
+import org.springframework.stereotype.Service;
+
 import service.SysAdminService;
 
+@Service
 public class SysAdminServiceImpl implements SysAdminService 
 {
 
